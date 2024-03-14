7ac9534ffe0c74851ee49f9fcd37c07b90225e73
max <derstadlmax@gmail.com>
2024-03-14 10:17:01 +0100

    changed calculate delay fn

--- project/src/Helpers/calculateDelay.js
+++ project/src/Helpers/calculateDelay.js
@@ -1,13 +1,16 @@
 function calculateDelay(arrayLength) {
-  const maxArrayLength = 50;
+  const maxArrayLength = 70;
   const minArrayLength = 10;
-  const maxDelay = 500;
+  const maxDelay = 250;
   const minDelay = 0;
+  const defaultDelay = 20;
 
   if (arrayLength <= minArrayLength) {
     return maxDelay;
   } else if (arrayLength >= maxArrayLength) {
     return minDelay;
+  } else if (arrayLength === (minArrayLength + maxArrayLength) / 2) {
+    return defaultDelay;
   } else {
     const slope = (minDelay - maxDelay) / (maxArrayLength - minArrayLength);
     return slope * (arrayLength - minArrayLength) + maxDelay;
